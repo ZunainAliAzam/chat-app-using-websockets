7170b1908851de4de67f46563a653818d4fcfb9b
ZunainAliAzam <zunainazam456@gmail.com>
2024-06-27 14:10:17 +0500

    final commit

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const Port = process.env.Port || 4000;
-// app.get("", async () => {});
 
 const server = app.listen(Port, () => {
   console.log(`server running on port ${Port}`);
 });
+
 const io = require("socket.io")(server);
 
 const socketsConnected = new Set();
 
 const onConnected = (socket) => {
-  console.log("User is connected on", socket.id);
+  console.log("User is connected on:", socket.id);
   socketsConnected.add(socket.id);
-
-  io.emit("clients-total", socketsConnected.size);
+    io.emit("clients-total", socketsConnected.size);
 
   socket.on("disconnect", () => {
     console.log("Socket Disconnected", socket.id);
     socketsConnected.delete(socket.id);
     io.emit("clients-total", socketsConnected.size);
+  });
+
+  socket.on("message", (data) => {
+    // console.log(data)
+    socket.broadcast.emit("chat-message", data);
+  });
+
+  socket.on("feedback", (data) => {
+    socket.broadcast.emit("feedback", data);
   });
 };
 
