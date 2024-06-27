5e262595bff96704e243d7db622d885a2e7c7421
ZunainAliAzam <zunainazam456@gmail.com>
2024-06-27 13:28:04 +0500

    socket connection and disconnection handling

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const Port = process.env.Port || 4000;
-
 // app.get("", async () => {});
 
 const server = app.listen(Port, () => {
   console.log(`server running on port ${Port}`);
 });
+const io = require("socket.io")(server);
+
+const socketsConnected = new Set();
+
+const onConnected = (socket) => {
+  console.log("User is connected on", socket.id);
+  socketsConnected.add(socket.id);
+
+  io.emit("clients-total", socketsConnected.size);
+
+  socket.on("disconnect", () => {
+    console.log("Socket Disconnected", socket.id);
+    socketsConnected.delete(socket.id);
+    io.emit("clients-total", socketsConnected.size);
+  });
+};
+
+io.on("connection", onConnected);
+
 app.use(express.static(path.join(__dirname, "public")));
